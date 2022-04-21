06401f6b84ea07f8aec8c3d92a5639e3eef5f6e4
Abdullah Fikri Handi Saputra <32560093+fikrihandy@users.noreply.github.com>
2022-04-21 09:04:17 +0700

    Add files via upload

--- lib/page/stateless_widget/cara_donasi.dart
+++ lib/page/stateless_widget/cara_donasi.dart
@@ -10,82 +10,87 @@ class CaraDonasi extends StatelessWidget {
         title: const Text('Cara Donasi'),
       ),
       body: SafeArea(
-          child: Center(
-        child: SingleChildScrollView(
-            child: Column(children: [
-          Container(
-            padding: const EdgeInsets.all(20),
-            width: double.infinity,
-            child: const Text('1. Transfer donasi ke nomor rekening berikut :'),
-          ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    width: 170,
-                    child: Column(
-                      children: [
-                        Image.network(
-                          'https://mwahyunz.id/007/2021/08/BSI-Bank-Syariah-Indonesia-1000x563.png',
-                          fit: BoxFit.fill,
-                        ),
-                        const SizedBox(height: 5),
-                        const Center(
-                          child: Text('BSI = 8666766750'),
-                        )
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 170,
-                    child: Column(
-                      children: [
-                        Image.network(
-                          'https://ika.um.ac.id/wp-content/uploads/2019/06/Logo-BRI.jpg',
-                          fit: BoxFit.fill,
-                        ),
-                        const SizedBox(height: 5),
-                        const Center(
-                          child: Text('BRI = 6950-01-024901-53-4'),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Center(
-                child: Text(
-                  'atas nama : MARKAZ AITAM AR-RASYID',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  child: const Text(
+                      '1. Transfer donasi ke nomor rekening berikut :'),
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              )
-            ],
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: 170,
+                          child: Column(
+                            children: [
+                              Image.network(
+                                'https://mwahyunz.id/007/2021/08/BSI-Bank-Syariah-Indonesia-1000x563.png',
+                                fit: BoxFit.fill,
+                              ),
+                              const SizedBox(height: 5),
+                              const Center(
+                                child: Text('BSI = 8666766750'),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 170,
+                          child: Column(
+                            children: [
+                              Image.network(
+                                'https://ika.um.ac.id/wp-content/uploads/2019/06/Logo-BRI.jpg',
+                                fit: BoxFit.fill,
+                              ),
+                              const SizedBox(height: 5),
+                              const Center(
+                                child: Text('BRI = 6950-01-024901-53-4'),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Center(
+                      child: Text(
+                        'atas nama : MARKAZ AITAM AR-RASYID',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    )
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  child: const Text(
+                      '2. Simpan bukti transaksi (screenshot/gambar kamera)'),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(right: 20, left: 20),
+                  width: double.infinity,
+                  child: const Text(
+                      '3. Konfirmasi dengan klik tombol "Donasi Sekarang"'),
+                ),
+                const SizedBox(
+                  height: 150,
+                )
+              ],
+            ),
           ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            width: double.infinity,
-            child: const Text(
-                '2. Simpan bukti transaksi (screenshot/gambar kamera)'),
-          ),
-          Container(
-            padding: const EdgeInsets.only(right: 20, left: 20),
-            width: double.infinity,
-            child: const Text(
-                '3. Konfirmasi dengan klik tombol "Donasi Sekarang"'),
-          ),
-          const SizedBox(
-            height: 150,
-          )
-        ])),
-      )),
+        ),
+      ),
     );
   }
 }
